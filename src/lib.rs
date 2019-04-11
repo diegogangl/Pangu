cbe5f8b9bfd6eb4e647a7e2028e367f12ec93b0c
Diego Garcia Gangl <dnicolas@gmail.com>
2019-04-11 11:00:43 -0300

    Make terrain structs and aliases public

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
 #![feature(test)]
 
 mod terrain;
+
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+
+/// Glue function to generate a terrain. Builds a terrain::Procedural
+/// and calls its build_mesh() function.
+#[pyfunction]
+fn procedural_terrain() -> (terrain::Faces, terrain::Vertices) {
+
+    terrain::Procedural::new().build_mesh()
+
+}
+
+
+/// The pangu module to be used in Python
+#[pymodule]
+fn pangu(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(procedural_terrain))?;
+
+    Ok(())
+}
