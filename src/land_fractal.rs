90d7ec93a45977777ac1bdcb6a7f7bb0be8cb0e1
Diego Garcia Gangl <dnicolas@gmail.com>
2019-04-15 20:42:44 -0300

    Use 6 octaves instead of 8
    Seems like the detail is far too small to be useful

--- src/land_fractal.rs
+++ src/land_fractal.rs
@@ -14,7 +14,7 @@ pub struct LandFractal {
 impl LandFractal {
     pub const DEFAULT_SEED: u32 = 0;
 
-    const OCTAVES: usize = 8;
+    const OCTAVES: usize = 6;
     const LACUNARITY: f64 = std::f64::consts::PI * 2.0 / 3.0;
     const PERSISTENCE: f64 = 0.5;
 
@@ -65,53 +65,39 @@ impl NoiseFn<Point2<f64>> for LandFractal {
 
         result = base;
 
-        // Octave 1
+        // Octave 1 - Large details
         let point2 =  [point[0] * Self::LACUNARITY, point[1] * Self::LACUNARITY];
         let octave1 = self.sources[1].get(point2) * Self::PERSISTENCE;
 
         result += octave1;
 
-        // Octave 2
+        // Octave 2 - Large details breakup
         let point3 =  [point2[0] * Self::LACUNARITY, point2[1] * Self::LACUNARITY];
         let mut octave2 = self.sources[2].get(point3);
         octave2 *= Self::PERSISTENCE.powi(2);
 
         result += octave2;
 
-        // Octave 3
+        // Octave 3 - Medium details
         let point4 =  [point3[0] * Self::LACUNARITY, point3[1] * Self::LACUNARITY];
         let mut octave3 = self.sources[3].get(point4);
         octave3 *= Self::PERSISTENCE.powi(3);
 
         result += octave3;
 
-        // Octave 4
+        // Octave 4 - Smaller details
         let point5 =  [point4[0] * Self::LACUNARITY, point4[1] * Self::LACUNARITY];
         let mut octave4 = self.sources[4].get(point5);
         octave4 *= Self::PERSISTENCE.powi(4);
 
         result += octave4;
 
-        // Octave 5
+        // Octave 5 - Fine details
         let point6 =  [point5[0] * Self::LACUNARITY, point5[1] * Self::LACUNARITY];
         let mut octave5 = self.sources[5].get(point6);
         octave5 *= Self::PERSISTENCE.powi(5);
 
         result += octave5;
-
-        // Octave 6
-        let point7 =  [point6[0] * Self::LACUNARITY, point6[1] * Self::LACUNARITY];
-        let mut octave6 = self.sources[6].get(point7);
-        octave6 *= Self::PERSISTENCE.powi(6);
-
-        result += octave6;
-
-        // Octave 7
-        let point8 =  [point7[0] * Self::LACUNARITY, point7[1] * Self::LACUNARITY];
-        let mut octave7 = self.sources[7].get(point8);
-        octave7 *= Self::PERSISTENCE.powi(7);
-
-        result += octave7;
 
         (result / self.scale) + 0.5
     }
