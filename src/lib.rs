dcbcae54374bed0cf2f8dac6ca02bb5752b09337
Diego Garcia Gangl <dnicolas@gmail.com>
2019-04-19 11:26:03 -0300

    Pass seed parameter from Python

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,29 @@
 #![feature(test)]
 
-mod terrain;
 mod land_fractal;
+mod terrain;
 
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
 use pyo3::wrap_pyfunction;
+
+
+type PyMesh = PyResult<(terrain::Faces, terrain::Vertices)>;
 
 
 /// Glue function to generate a terrain. Builds a terrain::Procedural
 /// and calls its build_mesh() function.
 #[pyfunction]
-fn procedural_terrain() -> (terrain::Faces, terrain::Vertices) {
+fn procedural_terrain(params: &PyDict) -> PyMesh {
+    let seed = match params.get_item("seed") {
+        Some(item) => item.extract()?,
+        None => 0,
+    };
 
-    terrain::Procedural::new()
-                    .set_rows(128)
-                    .set_columns(128)
-                    .set_seed(20)
-                    .build_mesh()
-
+    Ok(terrain::Procedural::new().set_rows(128)
+                                 .set_columns(128)
+                                 .set_seed(seed)
+                                 .build_mesh())
 }
 
 
