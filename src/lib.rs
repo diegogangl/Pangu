58d135aed3ce6c15bfb3abf1e77a9b81f5c3f5cf
Diego Garcia Gangl <dnicolas@gmail.com>
2019-04-19 15:01:25 -0300

    Add macro to get params from pydict

--- src/lib.rs
+++ src/lib.rs
@@ -11,36 +11,33 @@ use pyo3::wrap_pyfunction;
 type PyMesh = PyResult<(terrain::Faces, terrain::Vertices)>;
 
 
+/// Macro to extract item values from Python dictionary as simple
+/// rust types.
+///
+/// # Arguments
+///
+/// * `params` - The parameters dictionary
+/// * `key` - The key to look for in the dictionary
+/// * `default_value` - Value to use when the key is not found
+macro_rules! get_param {
+    ($params:expr, $key:expr, $default_value:expr) => {
+        match $params.get_item($key) {
+            Some(item) => item.extract()?,
+            None => $default_value,
+        };
+    };
+}
+
+
 /// Glue function to generate a terrain. Builds a terrain::Procedural
 /// and calls its build_mesh() function.
 #[pyfunction]
 fn procedural_terrain(params: &PyDict) -> PyMesh {
-    let seed = match params.get_item("seed") {
-        Some(item) => item.extract()?,
-        None => 0,
-    };
-
-    let rows = match params.get_item("rows") {
-        Some(item) => item.extract()?,
-        None => 64,
-    };
-
-    let columns = match params.get_item("columns") {
-        Some(item) => item.extract()?,
-        None => 64,
-    };
-
-
-    let offset_x = match params.get_item("offset_x") {
-        Some(item) => item.extract()?,
-        None => 0.0,
-    };
-
-
-    let offset_y = match params.get_item("offset_y") {
-        Some(item) => item.extract()?,
-        None => 0.0,
-    };
+    let seed = get_param!(params, "seed", 0);
+    let rows = get_param!(params, "rows", 64);
+    let columns = get_param!(params, "columns", 64);
+    let offset_x = get_param!(params, "offset_x", 0.0);
+    let offset_y = get_param!(params, "offset_y", 0.0);
 
 
     Ok(terrain::Procedural::new().set_rows(rows)
