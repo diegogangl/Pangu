1bcd092e545789bab0fd3a4ba219ee5a27dc3486
Diego Garcia Gangl <dnicolas@gmail.com>
2019-04-19 12:32:42 -0300

    Add offset parameters to terrain

--- src/lib.rs
+++ src/lib.rs
@@ -31,8 +31,22 @@ fn procedural_terrain(params: &PyDict) -> PyMesh {
     };
 
 
+    let offset_x = match params.get_item("offset_x") {
+        Some(item) => item.extract()?,
+        None => 0.0,
+    };
+
+
+    let offset_y = match params.get_item("offset_y") {
+        Some(item) => item.extract()?,
+        None => 0.0,
+    };
+
+
     Ok(terrain::Procedural::new().set_rows(rows)
                                  .set_columns(columns)
+                                 .set_offset_x(offset_x)
+                                 .set_offset_y(offset_y)
                                  .set_seed(seed)
                                  .build_mesh())
 }
