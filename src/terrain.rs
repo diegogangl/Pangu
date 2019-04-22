be9c831bb91854ef606b5e041156fdaebd5f1a0c
Diego Garcia Gangl <dnicolas@gmail.com>
2019-04-22 14:43:49 -0300

    Simplify code for step calculation
    The only things that matter are the ratio and scale, not the actual
    bounds.

--- src/terrain.rs
+++ src/terrain.rs
@@ -120,11 +120,11 @@ impl Procedural {
 
     /// Pre-calculate useful numbers for noise generation
     pub fn setup(self) -> Self {
-        let x_bounds = (0.0, 2.0);
-        let y_bounds = (0.0, 2.0);
+        let x_bounds = 2.0;
+        let y_bounds = 2.0;
 
-        let step_x = x_bounds.0 + (x_bounds.1 - x_bounds.0) / f64::from(self.columns);
-        let step_y = y_bounds.0 + (y_bounds.1 - y_bounds.0) / f64::from(self.rows);
+        let step_x = x_bounds / f64::from(self.columns);
+        let step_y = y_bounds / f64::from(self.rows);
 
         Procedural { step_x, step_y, ..self }
     }
