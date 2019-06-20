d9148ec2d017c75218c43056ddc1286ef06c71b2
Diego Garcia Gangl <dnicolas@gmail.com>
2019-06-19 21:24:03 -0300

    Better separator for logs

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,7 @@ macro_rules! param {
 
 /// Setup and return configuration
 fn get_config(params: &PyDict) -> Result<terrain::ProceduralConfig, PyErr> {
-    debug!("Start Terrain Generation =====================================");
+    debug!("[Start Terrain Generation]");
 
     let config = terrain::ProceduralConfig {
         seed: param!(params, "seed", DEFAULT_SEED),
