b63f672584213f16ef2eaf7124ddbd6a929b6c05
Januz <dnicolas@gmail.com>
2019-08-29 10:55:39 -0300

    Run rustfmt on map module

--- src/map.rs
+++ src/map.rs
@@ -3,8 +3,8 @@
 
 extern crate test;
 
-use std::ops::{Index, IndexMut};
 use std::fmt;
+use std::ops::{Index, IndexMut};
 
 type Coords = (usize, usize);
 
@@ -33,19 +33,19 @@ macro_rules! map2D {
 /// Neighborhoods to use with iterators. They don't include
 /// the center cell.
 pub mod neighbors {
-    
+
     // Moore Neighborhood
-    pub const MOORE: [(isize, isize); 6] = [(-1, -1), ( 0, -1), (-1,  1), 
-                                            ( 1,  0), ( 0,  1), ( 1,  1)];
+    pub const MOORE: [(isize, isize); 6] =
+        [(-1, -1), (0, -1), (-1, 1), (1, 0), (0, 1), (1, 1)];
 
     // Rotated Von Neumann Neighborhood
-    pub const VON_NEUMANN: [(isize, isize); 4]  = [(-1,  1),  (1,  1), 
-                                                   (-1, -1),  (1, -1)];
+    pub const VON_NEUMANN: [(isize, isize); 4] =
+        [(-1, 1), (1, 1), (-1, -1), (1, -1)];
 }
 
 
 /// Represents a 2D vector for floating point values
-/// 
+///
 /// This struct holds a 2D vector in row-major order, with
 /// all rows stored contiguos in memory. X represents rows,
 /// while Y represents columns.
@@ -55,20 +55,19 @@ pub mod neighbors {
 /// x=1   4      5     6
 /// x=2   7      8     9       
 ///
-/// If the struct is constructed with `with_size` it will 
+/// If the struct is constructed with `with_size` it will
 /// be initialized with 0.0.
 #[derive(Clone)]
 pub struct Map2D {
-
     // The vector containing the elements
     contents: Vec<f64>,
 
     // Width of each row
-    width: usize
+    width: usize,
 }
 
 
-impl Index<usize> for Map2D  {
+impl Index<usize> for Map2D {
     type Output = [f64];
 
     /// Immutable index implementation
@@ -80,8 +79,8 @@ impl Index<usize> for Map2D  {
     /// * `row` - The row to index
     ///
     /// # Panics
-    /// 
-    /// Panics if the selected row is larger than the total rows. 
+    ///
+    /// Panics if the selected row is larger than the total rows.
     fn index(&self, row: usize) -> &[f64] {
         assert!(row < self.height());
         let pos = row * self.width;
@@ -92,10 +91,9 @@ impl Index<usize> for Map2D  {
 
 
 impl IndexMut<usize> for Map2D {
-    
     /// Mutable index implementation
     ///
-    /// The mutable version of [the immutable index](Map2D::index) 
+    /// The mutable version of [the immutable index](Map2D::index)
     fn index_mut(&mut self, row: usize) -> &mut [f64] {
         assert!(row < self.height());
         let pos = row * self.width;
@@ -106,15 +104,18 @@ impl IndexMut<usize> for Map2D {
 
 
 impl fmt::Debug for Map2D {
-
     /// Debug format implementation
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "\nWidth: {}\
-                   \nHeight: {}\
-                   \nTotal: {}\
-                   \nData: ",
-                   self.width, self.height(),
-                   self.contents.len())?;
+        write!(
+            f,
+            "\nWidth: {}\
+             \nHeight: {}\
+             \nTotal: {}\
+             \nData: ",
+            self.width,
+            self.height(),
+            self.contents.len()
+        )?;
 
         for i in 0..self.height() {
             let row = &self[i];
@@ -123,16 +124,18 @@ impl fmt::Debug for Map2D {
 
         Ok(())
     }
-
 }
 
 
 impl Map2D {
-
     /// Return a new Map2D with no size or elements
     pub fn new() -> Self {
-        Map2D { contents: Vec::new(), width: 0 }
+        Map2D {
+            contents: Vec::new(),
+            width: 0,
+        }
     }
+
 
     /// Return a new Map2D with a size
     ///
@@ -144,19 +147,24 @@ impl Map2D {
     /// * `height` - Height of the 2D vector
     pub fn with_size(width: usize, height: usize) -> Self {
         assert!(width > 0);
-        Map2D { contents: vec![0.0; width * height], 
-                width: width }
+        Map2D {
+            contents: vec![0.0; width * height],
+            width: width,
+        }
     }
+
 
     /// Return the width
     pub fn width(&self) -> usize {
         self.width
     }
 
+
     /// Return the height
     pub fn height(&self) -> usize {
         self.contents.len() / self.width
     }
+
 
     /// Add an entire row to the map
     ///
@@ -195,15 +203,14 @@ impl Map2D {
     /// Returns X and Y coordinates
     pub fn iter_indices(&self) -> impl Iterator<Item = (usize, usize)> {
         let height = self.height();
-    
-        (0..self.contents.len())
-            .map(move |i| (i / height, i % height))
+
+        (0..self.contents.len()).map(move |i| (i / height, i % height))
     }
 
 
     /// Get neighbor coordinates without checks
     ///
-    /// This function takes an origin oordinate and a direction. 
+    /// This function takes an origin oordinate and a direction.
     /// There are no checks for whether this is inside the grid, or
     /// if the arithmetic will overflow. Use with caution!
     ///
@@ -217,16 +224,16 @@ impl Map2D {
     /// This will panic if the arithmetic overflows
     pub fn find(&self, origin: Coords, step: (isize, isize)) -> Coords {
         let target_x = if step.0 < 0 {
-                origin.0 - (step.0.abs() as usize)
-            } else {
-                origin.0 + (step.0 as usize)
-            };
+            origin.0 - (step.0.abs() as usize)
+        } else {
+            origin.0 + (step.0 as usize)
+        };
 
         let target_y = if step.1 < 0 {
-                origin.1 + (step.1 as usize)
-            } else {
-                origin.1 - (step.1.abs() as usize)
-            };
+            origin.1 + (step.1 as usize)
+        } else {
+            origin.1 - (step.1.abs() as usize)
+        };
 
         (target_x, target_y)
     }
@@ -234,8 +241,8 @@ impl Map2D {
 
     /// Get neighbor coordinates safely
     ///
-    /// This function takes a set of coordinates and a direction. 
-    /// If the target coordinates are found inside the map, it returns 
+    /// This function takes a set of coordinates and a direction.
+    /// If the target coordinates are found inside the map, it returns
     /// the neighbor coordinates. Otherwise it returns
     /// `None`. It also returns `None` in case of overflows.
     ///
@@ -243,35 +250,37 @@ impl Map2D {
     ///
     /// * `origin` - Tuple of X and Y coordinates
     /// * `step` - Tuple of directions in X and Y (eg. -1, 1)
-    pub fn safe_find(&self, origin: Coords, step: (isize, isize)) -> Option<Coords> {
-
+    pub fn safe_find(
+        &self,
+        origin: Coords,
+        step: (isize, isize),
+    ) -> Option<Coords> {
         let target_x = if step.0 < 0 {
-                origin.0.checked_sub(step.0.abs() as usize)
-            } else {
-                origin.0.checked_add(step.0 as usize)
-            };
+            origin.0.checked_sub(step.0.abs() as usize)
+        } else {
+            origin.0.checked_add(step.0 as usize)
+        };
 
 
         let target_y = if step.1 < 0 {
-                origin.1.checked_sub(step.1.abs() as usize)
-            } else {
-                origin.1.checked_add(step.1 as usize)
-            };
+            origin.1.checked_sub(step.1.abs() as usize)
+        } else {
+            origin.1.checked_add(step.1 as usize)
+        };
 
 
         match (target_x, target_y) {
-            (Some(x), Some(y)) => { 
+            (Some(x), Some(y)) => {
                 if self.is_inside(x, y) {
-                    Some((x, y)) 
+                    Some((x, y))
                 } else {
                     None
                 }
-            },
-            _ => None
+            }
+            _ => None,
         }
     }
 }
-
 
 
 #[cfg(test)]
@@ -316,16 +325,14 @@ mod tests {
         let test_map = map2D![0.0, 1.0, 2.0;
                               3.0, 4.0, 5.0;
                               6.0, 7.0, 8.0];
-    
-        for (x,y) in test_map.iter_indices() {
+
+        for (x, y) in test_map.iter_indices() {
             test_map[x][y] = 1.0;
         }
 
-    
-        for (x,y) in test_map.iter_indices() {
+
+        for (x, y) in test_map.iter_indices() {
             assert_eq!(1.0, test_map[x][y]);
         }
     }
-
-
 }
