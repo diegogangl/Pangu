646490d76aca0250f2be348c9465e21fefbf2105
Diego Garcia Gangl <dnicolas@gmail.com>
2020-06-18 11:02:42 -0300

    Organize, refactor and document island

--- src/modifiers/island.rs
+++ src/modifiers/island.rs
@@ -14,6 +14,8 @@ use rand::SeedableRng;
 
 /// Island modifier
 ///
+/// Applies a mask to flatten some areas of the terrain,
+/// generating an island in the middle.
 #[derive(Clone, Debug)]
 pub struct Island {
     /// Enable the modifier
@@ -21,49 +23,56 @@ pub struct Island {
 
     /// Invert Slope
     pub invert: bool,
+
+    /// Perlin noises
+    perlin: Vec<Perlin>,
+
+    /// Maximum distance. Used to normalize the radial gradient.
+    max_dist: f64,
+
+    /// Center of the radial gradient
+    center: [f64; 2],
+
+    /// The island mask
+    mask: Map2D<f64>,
 }
+
 
 impl Modifier for Island {
     fn is_enabled(&self) -> bool { false }
 
     fn run(&mut self, hmap: &mut Map2D<f64>) {
-        let rows = hmap.height();
-        let columns = hmap.width();
-        let center_x = (rows / 2) as f64;
-        let center_y = (columns / 2) as f64;
 
-        let mut mask = Map2D::with_size(rows, columns, 0.0);
-        let max_dist = math::distance([center_x, center_y], [(columns - 20) as f64, (rows - 20) as f64]);
+        // Counting on mask and hmap having the same number of
+        // values, otherwise it's crash time!
+        for (x, y) in hmap.iter_indices() {
 
-        let mut perlin: Vec<Perlin> = Vec::with_capacity(6);
-
-        for i in 0..5 {
-            perlin.push(Perlin::new().set_seed(0 + i));
-        }
-
-
-        for (x, y) in mask.iter_indices() {
-
-            let mut result = 0.0;
+            let mut noise = 0.0;
             let mut current_point = [x as f64 * 0.015, y as f64 * 0.015];
             let mut amplitude = 1.0;
 
             for i in 0..5 {
-                result += perlin[i].get(current_point) * amplitude;
-                current_point = [current_point[0] * 3.0, current_point[1] * 3.0];
+                noise += self.perlin[i].get(current_point) * amplitude;
+                current_point = [current_point[0] * 3.0,
+                                 current_point[1] * 3.0];
+
                 amplitude /= 5.0;
             }
 
-            result = math::bright_contrast(result, 2.0, 1.0);
-            let dist = math::distance([center_x, center_y],
-                                      [x as f64, y as f64]);
-            let normalized = (dist.sqrt() / max_dist.sqrt()).min(1.0);
-            mask[x][y] = math::lerp(0.0, 1.0, normalized.powi(5));
-            mask[x][y] = math::clamp(mask[x][y] - result, 0.0, 1.0);
-        }
+            // Adjust resulting noise
+            noise = math::bright_contrast(noise, 2.0, 1.0);
 
-        for (x, y) in hmap.iter_indices() {
-            hmap[x][y] *= mask[x][y];
+            // Generate radial gradient
+            let radial = {
+                let dist = math::distance(self.center, [x as f64, y as f64]);
+                let normalized = (dist.sqrt() / self.max_dist.sqrt()).min(1.0);
+
+                math::lerp(0.0, 1.0, normalized.powi(5))
+            };
+
+            // Substract noise from radial and apply mask
+            self.mask[x][y] = math::clamp(radial - noise, 0.0, 1.0);
+            hmap[x][y] *= self.mask[x][y];
         }
     }
 }
@@ -72,9 +81,29 @@ impl Modifier for Island {
 impl Island {
 
     pub fn new(params: &PyDict) -> PyResult<Self> {
+        let rows: f64 = get!(params, "rows");
+        let columns: f64 = get!(params, "columns");
+
+        // Initialize needed values
+        let center_x = rows / 2.0;
+        let center_y = columns / 2.0;
+        let max_dist = math::distance([center_x, center_y],
+                                      [columns - 20.0, rows - 20.0]);
+
+        // Initialize noise generators
+        let mut perlin: Vec<Perlin> = Vec::with_capacity(6);
+
+        for i in 0..5 {
+            perlin.push(Perlin::new().set_seed(0 + i));
+        }
+
         Ok(Island {
             enabled: get!(params, "enabled"),
             invert: get!(params, "invert"),
+            mask: Map2D::with_size(rows as usize, columns as usize, 0.0),
+            center: [center_x, center_y],
+            max_dist: max_dist,
+            perlin: perlin,
         })
     }
 }
