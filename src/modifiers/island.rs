74538440559c8afc4fb27c54af60a0ec769fe586
Diego Garcia Gangl <dnicolas@gmail.com>
2020-06-19 19:58:32 -0300

    Add settings for island
    And improve the algorithm a bit

--- src/modifiers/island.rs
+++ src/modifiers/island.rs
@@ -1,15 +1,11 @@
 use super::Modifier;
-use super::super::map::{Map2D, neighbors};
+use super::super::map::Map2D;
 use super::super::math;
 
-use noise::{NoiseFn, Perlin, Point2, Seedable};
+use noise::{NoiseFn, Perlin, Seedable};
 
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
-
-use rand::distributions::{Distribution, Uniform};
-use rand::rngs::StdRng;
-use rand::SeedableRng;
 
 
 /// Island modifier
@@ -21,12 +17,20 @@ pub struct Island {
     /// Enable the modifier
     pub enabled: bool,
 
-    pub val_a: f64,
-    pub val_b: f64,
-    pub val_c: f64,
-    pub val_d: f64,
-    pub val_e: f64,
-    pub val_f: f64,
+    /// Slope for the island
+    pub slope: f64,
+
+    /// Total elevation for the island
+    pub elevation: f64,
+
+    /// Noise size
+    pub breakup: f64,
+
+    /// Roughness of the noise
+    pub roughness: f64,
+
+    /// Intensity of the noise breaking the circle shape
+    pub noise_intensity: f64,
 
     /// Perlin noises
     perlin: Vec<Perlin>,
@@ -52,33 +56,36 @@ impl Modifier for Island {
         for (x, y) in hmap.iter_indices() {
 
             let mut noise = 0.0;
-            let mut current_point = [x as f64 * self.val_d / 100.0, y as f64 * self.val_d / 100.0];
-            let mut amplitude = 1.0;
-
+            let mut amplitude = 1.1 - (self.breakup / 100.0) ;
+            let mut current_point = [x as f64 * self.breakup / 100.0,
+                                     y as f64 * self.breakup / 100.0];
 
             for i in 0..5 {
-                let warp = self.perlin[1].get(current_point) / 10.0;
                 noise += self.perlin[i].get(current_point) * amplitude;
-                current_point = [current_point[0] * self.val_f + warp,
-                                 current_point[1] * self.val_f + warp];
+                current_point = [current_point[0] * 4.0,
+                                 current_point[1] * 4.0];
 
-                amplitude /= self.val_e;
+                amplitude /= self.roughness;
             }
 
             // Generate radial gradient
             let radial = {
                 let dist = math::distance(self.center, [x as f64, y as f64]);
                 let normalized = (dist.sqrt() / self.max_dist.sqrt()).min(1.0);
-
-                math::lerp(0.0, 1.0, normalized.powf(self.val_c))
+                math::lerp(0.0, 1.0, normalized.powf(3.0))
             };
 
-
             // Adjust resulting noise
-            noise = math::bright_contrast(noise, self.val_a, self.val_b);
+            noise = math::lerp(0.0, 1.0, noise);
+            noise *= self.noise_intensity;
+
+            let mut mask = math::clamp(radial - noise, 0.0, 1.0);
+            mask *= self.elevation;
+            mask = mask.powf(self.slope);
+
+            self.mask[x][y] = mask;
 
             // Substract noise from radial and apply mask
-            self.mask[x][y] = math::clamp(noise, 0.0, 1.0);
             hmap[x][y] *= self.mask[x][y];
         }
     }
@@ -97,8 +104,10 @@ impl Island {
         // Initialize needed values
         let center_x = rows / 2.0;
         let center_y = columns / 2.0;
+        let mut size: f64 = get!(params, "size");
+        size = math::remap([0.0, 100.0], size, [columns / 2.5, 1.0]);
         let max_dist = math::distance([center_x, center_y],
-                                      [columns - 20.0, rows - 20.0]);
+                                      [columns - size, rows - size]);
 
         // Initialize noise generators
         let mut perlin: Vec<Perlin> = Vec::with_capacity(6);
@@ -107,18 +116,34 @@ impl Island {
             perlin.push(Perlin::new().set_seed(seed + i));
         }
 
+        // Convert parameters
+        let mut elevation: f64 = get!(params, "elevation");
+        elevation = math::remap([0.0, 100.0], elevation, [0.0, 1.0]);
+
+        let mut breakup: f64 = get!(params, "breakup");
+        breakup = math::remap([0.0, 100.0], breakup, [0.0, 10.0]);
+
+        let mut roughness: f64 = get!(params, "roughness");
+        roughness = math::remap([0.0, 100.0], roughness, [6.0, 3.0]);
+
+        let mut noise_intensity: f64 = get!(params, "noise_intensity");
+        noise_intensity = math::remap([0.0, 100.0], noise_intensity, [0.0, 1.5]);
+
+        let mut slope: f64 = get!(params, "slope");
+        slope = math::remap([0.0, 100.0], slope, [0.2, 2.0]);
+
+
         Ok(Island {
             enabled: get!(params, "enabled"),
             mask: Map2D::with_size(rows as usize, columns as usize, 0.0),
             center: [center_x, center_y],
             max_dist: max_dist,
             perlin: perlin,
-            val_a: get!(params, "val_a"),
-            val_b: get!(params, "val_b"),
-            val_c: get!(params, "val_c"),
-            val_d: get!(params, "val_d"),
-            val_e: get!(params, "val_e"),
-            val_f: get!(params, "val_f"),
+            slope: slope,
+            elevation: elevation,
+            breakup: breakup,
+            roughness: roughness,
+            noise_intensity: noise_intensity
         })
     }
 }
