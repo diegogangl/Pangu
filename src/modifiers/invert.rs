e590c6cb0376156b02c10c9f42ae0a64bf261175
Diego Garcia Gangl <dnicolas@gmail.com>
2020-06-22 10:36:35 -0300

    Add factor setting to invert

--- src/modifiers/invert.rs
+++ src/modifiers/invert.rs
@@ -1,5 +1,6 @@
 use super::Modifier;
 use super::super::map::Map2D;
+use super::super::math;
 
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
@@ -10,11 +11,17 @@ use pyo3::types::PyDict;
 /// Inverts the terrain
 #[derive(Clone, Debug)]
 pub struct Invert {
+    /// Intensity of the inversion
+    pub factor: f64,
 }
 
 impl Invert {
     pub fn new(params: &PyDict) -> PyResult<Self> {
+        let mut factor: f64 = get!(params, "factor");
+        factor = math::remap([0.0, 100.0], factor, [1.0, -1.0]);
+
         Ok(Invert {
+            factor: factor,
         })
     }
 }
@@ -22,10 +29,17 @@ impl Invert {
 
 impl Modifier for Invert {
     fn run(&mut self, hmap: &mut Map2D<f64>) {
-        for i in 0..hmap.contents.len() {
-            hmap.contents[i] *= -1.0;
-            hmap.contents[i] += hmap.max;
 
+        // Recover height from the factor pushing the map down
+        let recover = if self.factor < 0.0 {
+            hmap.max * self.factor.abs()
+        } else {
+            0.0
+        };
+
+        for i in 0..hmap.contents.len() {
+            hmap.contents[i] *= self.factor;
+            hmap.contents[i] += recover;
         }
     }
 }
