3cffe082a53c075c10f92327b007a3cc464af73c
Diego Garcia Gangl <dnicolas@gmail.com>
2021-09-03 14:12:55 -0300

    Simpler method to build and extract from docker

--- scripts/extract_from_docker.sh
+++ scripts/extract_from_docker.sh
@@ -3,8 +3,8 @@
 # This script extracts libpangu.so from the docker image.
 # Needed to generate linux versions with ancient Glibc versions.
 
-docker create -ti --name dummy januz/pangu
-docker cp dummy:/target/release/libpangu.so .
-docker rm -fv dummy
+cd "$(dirname "$0")"
+docker build -t pangu ..
+docker run pangu cat /target/release/libpangu.so > ./libpangu.so
 mkdir -p ../target/linux_old_glibc/
 mv libpangu.so ../target/linux_old_glibc/pangu.so
